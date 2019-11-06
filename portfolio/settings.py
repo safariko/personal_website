3c65fba30fee99899aa3244f4ed5a5db0f1c6bc5
Safar Kurbansho <safariko93@gmail.com>
2019-11-05 23:49:52 -0600

    pics added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -138,9 +138,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-try:
-    from local_settings import *
-except ImportError:
-    pass
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
 
 
